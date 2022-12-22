b3032b040e048152c7f3ef7f8815770ddda80e47
jangjh45 <jangjh45@naver.com>
2022-12-22 14:41:59 +0900

    Initial commit

---
+++ src/main/java/com/practice/app1/RegisterController.java
@@ -0,0 +1,13 @@
+package com.practice.app1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegisterController {
+    @RequestMapping("/add")
+    public String register(){
+        return "a";
+    }
+}
