1b1c689dccbfe38b5ae8da402a83c256d8c15b87
jangjh45 <jangjh45@naver.com>
2022-12-26 21:56:34 +0900

    #1 - insert부분 MyBatis지우고 다시 구현
    MyBatis가 구현이 잘되지 않아서 다시 수정함

--- src/main/java/com/practice/app1/dao/UserDaoImpl.java
+++ src/main/java/com/practice/app1/dao/UserDaoImpl.java
@@ -1,18 +1,40 @@
 package com.practice.app1.dao;
 
 import com.practice.app1.domain.User;
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 @Repository
 public class UserDaoImpl implements UserDao {
     @Autowired
-    private SqlSession session;
-    private static String namespace = "com.practice.app1.dao.UserMapper.";
+    DataSource ds;
+
+    final int FAIL = 0;
 
     @Override
-    public int insert(User user) throws Exception {
-        return session.insert(namespace+"insert", user);
-    } // int insert(String statement, Object parameter)
+    public int insertUser(User user) {
+        int rowCnt = FAIL;
+
+        String sql = "insert into user values (?,?,?,?,?,now())";
+
+        try (Connection conn = ds.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);)
+        {
+            pstmt.setString(1, user.getId());
+            pstmt.setString(2, user.getPsw());
+            pstmt.setString(3, user.getName());
+            pstmt.setString(4, user.getEmail());
+            pstmt.setString(5, String.valueOf(new java.sql.Date(user.getBirth().getTime())));
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return FAIL;
+        }
+    }
 }
