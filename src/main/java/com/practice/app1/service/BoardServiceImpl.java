03d195831fe40163414c7982b63c3a099cafde23
jangjh45 <jangjh45@naver.com>
2022-12-28 00:47:30 +0900

    #6 - 27일 마무리

---
+++ src/main/java/com/practice/app1/service/BoardServiceImpl.java
@@ -0,0 +1,63 @@
+package com.practice.app1.service;
+
+import com.practice.app1.dao.BoardDao;
+import com.practice.app1.domain.BoardDto;
+import com.practice.app1.domain.SearchCondition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+    @Autowired
+    BoardDao boardDao;
+
+    @Override
+    public int getCount() throws Exception {
+        return boardDao.count();
+    }
+
+    @Override
+    public int remove(Integer bno, String writer) throws Exception {
+        return boardDao.delete(bno, writer);
+    }
+
+    @Override
+    public int write(BoardDto boardDto) throws Exception {
+        return boardDao.insert(boardDto);
+    }
+
+    @Override
+    public List<BoardDto> getList() throws Exception {
+        return boardDao.selectAll();
+    }
+
+    @Override
+    public BoardDto read(Integer bno) throws Exception {
+        BoardDto boardDto = boardDao.select(bno);
+        boardDao.increaseViewCnt(bno);
+        return boardDto;
+    }
+
+    @Override
+    public List<BoardDto> getPage(Map map) throws Exception {
+        return boardDao.selectPage(map);
+    }
+
+    @Override
+    public int modify(BoardDto boardDto) throws Exception {
+        return boardDao.update(boardDto);
+    }
+
+    @Override
+    public List<BoardDto> getSearchResultPage(SearchCondition sc) throws Exception{
+        return  boardDao.searchSelectPage(sc);
+    }
+
+    @Override
+    public  int getSearchResultCnt(SearchCondition sc) throws Exception{
+        return boardDao.searchResultCnt(sc);
+    }
+}
---
+++ src/test/java/com/practice/app1/domain/PageHandlerTest.java
@@ -0,0 +1,34 @@
+package com.practice.app1.domain;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PageHandlerTest {
+    @Test
+    public void test() {
+        PageHandler ph = new PageHandler(1000, 1);
+        ph.print();
+        System.out.println("ph = " + ph);
+        assertTrue(ph.getBeginPage() ==1);
+        assertTrue(ph.getEndPage() ==10);
+    }
+
+    @Test
+    public void test1() {
+        PageHandler ph = new PageHandler(1000, 11);
+        ph.print();
+        System.out.println("ph = " + ph);
+        assertTrue(ph.getBeginPage() ==11);
+        assertTrue(ph.getEndPage() ==20);
+    }
+
+    @Test
+    public void test2() {
+        PageHandler ph = new PageHandler(255, 25);
+        ph.print();
+        System.out.println("ph = " + ph);
+        assertTrue(ph.getBeginPage() ==21);
+        assertTrue(ph.getEndPage() ==26);
+    }
+}
