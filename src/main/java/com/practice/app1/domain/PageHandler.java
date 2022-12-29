3b4f0c451cdba19f33af815db66916c6c6780289
jangjh45 <jangjh45@naver.com>
2022-12-29 23:13:00 +0900

    #6 - 검색 기능 구현완료, 페이징 조금 더 보완해야함
    UriComponentsBuilder, getQueryString 공부 필요

--- src/main/java/com/practice/app1/domain/PageHandler.java
+++ src/main/java/com/practice/app1/domain/PageHandler.java
@@ -1,33 +1,41 @@
 package com.practice.app1.domain;
 
 public class PageHandler {
+
+//    private int page; // 현재 페이지
+//    private int pageSize; // 한 페이지의 크기
+//    private String option;
+//    private String keyword;
+    private SearchCondition sc; // 위에 4개 모음집
+
     private int totalCnt; // 총 게시물 갯수
-    private int pageSize; // 한 페이지의 크기
     private int naviSize = 10; // 페이지 내비게이션의 크기
-    private int page; // 현재 페이지
     private int totalPage; // 전체 페이지의 갯수
     private int beginPage; // 내비게이션의 첫번째 페이지
     private int endPage; // 내비게이션의 마지막 페이지
     private boolean showPrev; // 이전 페이지로 이동하는 링크를 보여줄지 것인지의 여부
     private boolean showNext; // 다음 페이지로 이동하는 링크를 보여줄 것인지의 여부
 
-    public PageHandler(int totalCnt, int page) {
-        this(totalCnt, page, 10);
-    }
-    public PageHandler(int totalCnt, int page, int pageSize) {
+    // BoardController -> PageHandler pageHandler = new PageHandler(totalCnt, sc); 이부분에서 받아옴.
+    public PageHandler(int totalCnt, SearchCondition sc) {
         this.totalCnt = totalCnt;
-        this.page = page;
-        this.pageSize = pageSize;
+        this.sc = sc;
 
-        totalPage = (int)Math.ceil(totalCnt / (double)pageSize); //  Math.ceil(소수값 존재할때 올림)
-        beginPage = (page-1) / naviSize * naviSize + 1;
+        doPaging(totalCnt, sc);
+    }
+
+    public void doPaging(int totalCnt, SearchCondition sc) {
+        this.totalCnt = totalCnt;
+
+        totalPage = (int)Math.ceil(totalCnt / (double)sc.getPageSize()); //  Math.ceil(소수값 존재할때 올림)
+        beginPage = (sc.getPage()-1) / naviSize * naviSize + 1;
         endPage = Math.min(beginPage + naviSize -1 , totalPage); // 둘 중 작은것을 씀
         showPrev = beginPage != 1;
         showNext = endPage != totalPage;
     }
 
     void print(){
-        System.out.println("page = " + page);
+        System.out.println("page = " + sc.getPage());
         System.out.print(showPrev ? "[PREV] " : "");
         for(int i = beginPage; i <= endPage; i++){
             System.out.print(i+" ");
@@ -35,19 +43,12 @@ public class PageHandler {
         System.out.println(showNext ? "[NEXT]" : "");
     }
 
-    @Override
-    public String toString() {
-        return "PageHandler{" +
-                "totalCnt=" + totalCnt +
-                ", pageSize=" + pageSize +
-                ", naviSize=" + naviSize +
-                ", page=" + page +
-                ", totalPage=" + totalPage +
-                ", beginPage=" + beginPage +
-                ", endPage=" + endPage +
-                ", showPrev=" + showPrev +
-                ", showNext=" + showNext +
-                '}';
+    public SearchCondition getSc() {
+        return sc;
+    }
+
+    public void setSc(SearchCondition sc) {
+        this.sc = sc;
     }
 
     public int getTotalCnt() {
@@ -58,28 +59,12 @@ public class PageHandler {
         this.totalCnt = totalCnt;
     }
 
-    public int getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(int pageSize) {
-        this.pageSize = pageSize;
-    }
-
     public int getNaviSize() {
         return naviSize;
     }
 
     public void setNaviSize(int naviSize) {
         this.naviSize = naviSize;
-    }
-
-    public int getPage() {
-        return page;
-    }
-
-    public void setPage(int page) {
-        this.page = page;
     }
 
     public int getTotalPage() {
@@ -120,5 +105,19 @@ public class PageHandler {
 
     public void setShowNext(boolean showNext) {
         this.showNext = showNext;
+    }
+
+    @Override
+    public String toString() {
+        return "PageHandler{" +
+                "sc=" + sc +
+                ", totalCnt=" + totalCnt +
+                ", naviSize=" + naviSize +
+                ", totalPage=" + totalPage +
+                ", beginPage=" + beginPage +
+                ", endPage=" + endPage +
+                ", showPrev=" + showPrev +
+                ", showNext=" + showNext +
+                '}';
     }
 }
